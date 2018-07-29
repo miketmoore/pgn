e1db649f33a3f52ac1921d9a4bd64ab35f18eca8
Michael Moore <michael.moore@contrastsecurity.com>
2018-07-29 10:51:13 -0400

    :ok_hand: cleanup

--- pgn.go
+++ pgn.go
@@ -2,7 +2,6 @@ package pgn
 
 import (
 	"bufio"
-	"fmt"
 	"regexp"
 	"strings"
 )
@@ -78,35 +77,17 @@ func parseSevenTagRoster(line string) (string, string, bool) {
 func parseMovetext(lines []string) Movetext {
 	mt := Movetext{}
 	str := strings.Join(lines, " ")
-	fmt.Println(str)
 
-	// split by numbers /[0-1]\.(.*)/
-	// x := strings.Split(str, ". ")
-
-	// for _, val := range x {
-	// 	fmt.Println(val)
-	// }
-
-	// re := regexp.MustCompile(`([0-1]\. )(.*)`)
-	// split := re.Split(str, -1)
-	// for _, val := range split {
-	// 	fmt.Println(val)
-	// }
-
-	// str := "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6"
 	re := regexp.MustCompile(`[1-9]\. `)
 	split := re.Split(str, -1)
-	// final := [][]string{}
+
 	for _, val := range split {
 		if strings.TrimSpace(val) != "" {
 			moves := strings.Split(strings.TrimSpace(val), " ")
 			for i := 0; i < len(moves); i++ {
 				moves[i] = strings.TrimSpace(moves[i])
 			}
-			fmt.Println(moves)
 
-			// final = append(final, moves)
-			// mt = append(mt, moves)
 			mt = append(mt, MovetextEntry{
 				White: moves[0],
 				Black: moves[1],
