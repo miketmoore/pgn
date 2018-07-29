e2f5d520e13d32b6ae38bb1c0d9caed59a3fedee
Michael Moore <michael.moore@contrastsecurity.com>
2018-07-29 08:03:11 -0400

    :sparkles: parse seven tag roster

--- pgn.go
+++ pgn.go
@@ -3,6 +3,7 @@ package pgn
 import (
 	"bufio"
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -20,32 +21,36 @@ func Parse(raw string) PGN {
 	pgn := PGN{}
 	for scanner.Scan() {
 		line := scanner.Text()
-		if val, ok := parseSevenTagRoster(line, "Event"); ok {
-			pgn.TagPairs.Event = val
-		} else if val, ok := parseSevenTagRoster(line, "Site"); ok {
-			pgn.TagPairs.Site = val
-		} else if val, ok := parseSevenTagRoster(line, "Date"); ok {
-			pgn.TagPairs.Date = val
-		} else if val, ok := parseSevenTagRoster(line, "Round"); ok {
-			pgn.TagPairs.Round = val
-		} else if val, ok := parseSevenTagRoster(line, "White"); ok {
-			pgn.TagPairs.White = val
-		} else if val, ok := parseSevenTagRoster(line, "Black"); ok {
-			pgn.TagPairs.Black = val
-		} else if val, ok := parseSevenTagRoster(line, "Result"); ok {
-			pgn.TagPairs.Result = val
+		key, val, ok := parseSevenTagRoster(line)
+		if ok {
+			switch key {
+			case "Event":
+				pgn.TagPairs.Event = val
+			case "Site":
+				pgn.TagPairs.Site = val
+			case "Date":
+				pgn.TagPairs.Date = val
+			case "Round":
+				pgn.TagPairs.Round = val
+			case "White":
+				pgn.TagPairs.White = val
+			case "Black":
+				pgn.TagPairs.Black = val
+			case "Result":
+				pgn.TagPairs.Result = val
+			}
 		}
 	}
 	return pgn
 }
 
-func parseSevenTagRoster(line, tag string) (string, bool) {
-	start := fmt.Sprintf("[%s", tag)
-	end := strings.Index(line, "]")
-	if strings.Index(line, start) == 0 && end > len(start) {
-		sub := line[6:]
-		sub = sub[2 : len(sub)-2]
-		return sub, true
+func parseSevenTagRoster(line string) (string, string, bool) {
+	var re = regexp.MustCompile(`\[(.*) "(.*)"\]`)
+	matches := re.FindStringSubmatch(line)
+	if len(matches) == 3 {
+		fmt.Println(matches[1])
+		fmt.Println(matches[2])
+		return matches[1], matches[2], true
 	}
-	return "", false
+	return "", "", false
 }
