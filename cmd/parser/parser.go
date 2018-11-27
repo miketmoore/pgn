79bc14a8c460639bb1b37919bd249e2c7cfa262e
Michael Moore <michael.moore@contrastsecurity.com>
2018-11-26 21:12:58 -0500

    main pkg

---
+++ cmd/parser/parser.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/miketmoore/pgn"
+)
+
+func main() {
+
+	file := flag.String("input", "", "Path to a *.pgn file containing zero or more games")
+	flag.Parse()
+
+	bytes, err := ioutil.ReadFile(*file)
+	if err != nil {
+		fmt.Printf("Error reading file: %s\n", *file)
+		os.Exit(1)
+	}
+
+	scanner := pgn.NewScanner(string(bytes))
+	lexer := pgn.NewLexer(scanner)
+
+	tokens := []pgn.Token{}
+	err, tokens = lexer.Tokenize(tokens)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	fmt.Println("Total tokens parsed: ", len(tokens))
+
+	os.Exit(0)
+}
