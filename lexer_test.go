83b35dce85fc16769f9e69d05ef55a51dc7def02
Michael Moore <michael.moore@contrastsecurity.com>
2018-12-02 15:30:40 -0500

    add tag pairs to lexer test

--- lexer_test.go
+++ lexer_test.go
@@ -377,8 +377,17 @@ func TestTokenize(t *testing.T) {
 			},
 		},
 		{
-			name: "Whole game of movetext",
-			in: "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 {This opening is called the Ruy Lopez.}" +
+			name: "Whole game",
+			in: "[Event \"F/S Return Match\"]\n" +
+				"[Site \"Belgrade, Serbia JUG\"]\n" +
+				"[Date \"1992.11.04\"]\n[Round \"29\"]\n" +
+				"[White \"Fischer, Robert J.\"]\n" +
+				"[Black \"Spassky, Boris V.\"]\n" +
+				"[Result \"1/2-1/2\"]\n" +
+				"[a \"\"]\n" +
+				"[A \"\"]\n" +
+				"\n" +
+				"1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 {This opening is called the Ruy Lopez.}" +
 				"4. Ba4 Nf6 5. O-O Be7 6. Re1 b5 7. Bb3 d6 8. c3 O-O 9. h3 Nb8 10. d4 Nbd7" +
 				"11. c4 c6 12. cxb5 axb5",
 			//13. Nc3 Bb7 14. Bg5 b4 15. Nb1 h6 16. Bh4 c5 17. dxe5",
@@ -388,6 +397,28 @@ func TestTokenize(t *testing.T) {
 			// "35. Ra7 g6 36. Ra6+ Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5 40. Rd6 Kc5 41. Ra6" +
 			// "Nf2 42. g4 Bd3 43. Re6 1/2-1/2",
 			out: buildTokens(
+				[]pgn.Token{
+					pgn.Token{Type: pgn.TokenTagName, Value: "Event"},
+					pgn.Token{Type: pgn.TokenTagValue, Value: "F/S Return Match"},
+					pgn.Token{Type: pgn.TokenTagName, Value: "Site"},
+					pgn.Token{Type: pgn.TokenTagValue, Value: "Belgrade, Serbia JUG"},
+					pgn.Token{Type: pgn.TokenTagName, Value: "Date"},
+					pgn.Token{Type: pgn.TokenTagValue, Value: "1992.11.04"},
+					pgn.Token{Type: pgn.TokenTagName, Value: "Round"},
+					pgn.Token{Type: pgn.TokenTagValue, Value: "29"},
+					pgn.Token{Type: pgn.TokenTagName, Value: "White"},
+					pgn.Token{Type: pgn.TokenTagValue, Value: "Fischer, Robert J."},
+					pgn.Token{Type: pgn.TokenTagName, Value: "Black"},
+					pgn.Token{Type: pgn.TokenTagValue, Value: "Spassky, Boris V."},
+					pgn.Token{Type: pgn.TokenTagName, Value: "Result"},
+					pgn.Token{Type: pgn.TokenTagValue, Value: "1/2-1/2"},
+					pgn.Token{Type: pgn.TokenTagName, Value: "a"},
+					pgn.Token{Type: pgn.TokenTagValue, Value: ""},
+					pgn.Token{Type: pgn.TokenTagName, Value: "A"},
+					pgn.Token{Type: pgn.TokenTagValue, Value: ""},
+					pgn.Token{Type: pgn.TokenTagName, Value: "_"},
+					pgn.Token{Type: pgn.TokenTagValue, Value: ""},
+				},
 				newMove("1", "", "e4", "", "e5"),
 				newMove("2", "N", "f3", "N", "c6"),
 				newMove("3", "B", "b5", "", "a6"),
